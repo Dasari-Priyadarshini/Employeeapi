980c1c98fe1f0bc4227e04393d7985a618854fe5
Dasari-Priyadarshini <priyadarshini1347@gmail.com>
2026-06-14 22:09:21 +0530

    Completed Task 2

---
+++ employeeapi/src/main/resources/db/migration/V2__insert_sample_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee(name, department, salary)
+VALUES ('Priya', 'IT', 35000);
+
+INSERT INTO employee(name, department, salary)
+VALUES ('Rahul', 'HR', 28000);.\mvnw spring-boot:run
